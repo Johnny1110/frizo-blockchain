478f4014b899167780199730e5971c6eda4ec0ff
Johnny.Wang <johnny.wang@btse.com>
2025-07-19 12:27:57 +0800

    feat: MPT insertIntoLeaf done

--- crypto/mpt.go
+++ crypto/mpt.go
@@ -1,3 +1,335 @@
 package crypto
 
-// Patricia Tree
+import (
+	"errors"
+	"frizo-blockchain/common"
+)
+
+// Modified Merkle Patricia Tree (MPT)
+
+type MPTNodeType uint8 // 1 byte
+
+func (nt MPTNodeType) String() string {
+	switch nt {
+	case LEAF:
+		return "LEAF"
+	case BRANCH:
+		return "BRANCH"
+	case EXTENSION:
+		return "EXT"
+	case BLANK:
+		return "BLANK"
+	}
+	return "<unknown>"
+}
+
+const (
+	// BLANK nil node
+	BLANK MPTNodeType = iota
+
+	// LEAF store actual value
+	// format: [path-encode, value(mandatory)]
+	LEAF
+
+	// EXTENSION path compress
+	// format: [shared_path, next-node-hash]
+	EXTENSION
+
+	// BRANCH
+	// format: [16 sub-node-hash, value(optional)]
+	BRANCH
+)
+
+// MPTNode support 4 MPTNodeType
+// include path info (hex encoding)
+// BRANCH node have 16 sub-node (not 2)
+type MPTNode struct {
+	NodeType MPTNodeType
+	Path     []byte
+	Value    []byte
+	Children [16]*MPTNode // only BRANCH node have Children
+	Child    *MPTNode     // only for EXTENSION node have 1 child
+	Hash     *common.Hash
+	Dirty    bool // remark is modified, optimized hash data
+}
+
+// ModifiedMerklePatriciaTree MPT main struct
+type ModifiedMerklePatriciaTree struct {
+	root     *MPTNode
+	db       map[common.Hash][]byte
+	hashFunc func([]byte) common.Hash
+}
+
+// NewMPT create new ModifiedMerklePatriciaTree
+func NewMPT() *ModifiedMerklePatriciaTree {
+	return &ModifiedMerklePatriciaTree{
+		root:     nil,
+		db:       make(map[common.Hash][]byte),
+		hashFunc: defaultHashFunc,
+	}
+}
+
+// ========== Encoding func ==========
+
+// HexToCompact convert hex path to Compact(緊湊編碼) for saving space
+// encoding rule:
+// - first nibble represent node type: [00=extension even, 01=extension odd, 10=leaf even, 11=leaf odd]
+func HexToCompact(hexPath []byte, isLeaf bool) []byte {
+	// terminator (leaf is 1, extension is 0)
+	terminator := byte(0) // default is extension (0000 0000)
+	if isLeaf {
+		terminator = 1 // leaf (0000 0001)
+	}
+
+	// process odd even hex path len (convert terminator)
+	// - ex: even leaf -> from 1 -> 10
+	// - ex: odd  leaf -> from 1 -> 11
+	if len(hexPath)%2 == 1 {
+		// odd len: first byte = (terminator*2 + 1) << 4 | hex[0]
+		// combined 2 hex to 1 compact
+		compact := make([]byte, len(hexPath)/2+1)     // why +1 ? because first one represent node-type
+		compact[0] = (terminator*2+1)<<4 | hexPath[0] // odd situation: merge first hex path into compact head. (terminator*2+1) -> 添加奇偶屬性
+		for i := 1; i < len(hexPath); i += 2 {        // skip first one, already filled first hexPath in previous line.
+			compact[i/2+1] = hexPath[i]<<4 | hexPath[i+1] // combined 2 hexPath to 1 compact [0x(hex_1 hex_2), 0x(hex_3 hex_4), 0x(hex_5 hex_6)]
+		}
+		return compact
+	} else {
+		// even len: first byte = terminator * 2 << 4
+		compact := make([]byte, len(hexPath)/2+1)
+		compact[0] = terminator * 2 << 4
+		for i := 0; i < len(hexPath); i += 2 {
+			compact[i/2+1] = hexPath[i]<<4 | hexPath[i+1] // combined 2 hexPath to 1 compact [0x(hex_1 hex_2), 0x(hex_3 hex_4), 0x(hex_5 hex_6)]
+		}
+		return compact
+	}
+}
+
+// CompactToHex revert compact to hexPath
+func CompactToHex(compact []byte) ([]byte, bool) {
+	if len(compact) == 0 {
+		return nil, false
+	}
+
+	// parse first byte
+	head := compact[0]
+	flag := head >> 4 // node type: [0000 0000=extension even, 0000 0001=extension odd, 0000 0010=leaf even, 0000 0011=leaf odd]
+
+	isLeaf := (flag & 2) != 0 // leaf is '0011' or '0010'
+	isOdd := (flag & 1) != 0  // odd is 'xxx1' even is 'xxx0'
+
+	if isOdd { // handle odd
+		hexPath := make([]byte, (len(compact)-1)*2+1)
+		// split first hexPath from compact[0] -> (xxxx xxxx) & (0000 1111)
+		hexPath[0] = compact[0] & 0x0f
+		for i := 1; i < len(compact); i++ {
+			// split 1 compact to 2 hex
+			hexPath[i*2-1] = compact[i] >> 4
+			hexPath[i*2] = compact[i] & 0x0f
+		}
+		return hexPath, isLeaf
+	} else { // handle even
+		hexPath := make([]byte, (len(compact)-1)*2) // compact len remove head then * 2
+		for i := 1; i < len(compact); i++ {
+			hexPath[i*2-2] = compact[i] >> 4
+			hexPath[i*2-1] = compact[i] & 0x0f
+		}
+		return hexPath, isLeaf
+	}
+}
+
+// KeyToHex convert key to hexPath
+func KeyToHex(key []byte) []byte {
+	// split 1 key byte to 2 nibble, ex: 0x12 -> 0001 0002
+	hexPath := make([]byte, len(key)*2)
+	for idx, val := range key {
+		hexPath[idx*2] = val >> 4
+		hexPath[idx*2+1] = val & 0x0f
+	}
+	return hexPath
+}
+
+// ========== MPT Core Access Func ==========
+
+func (t *ModifiedMerklePatriciaTree) Put(key, value []byte) error {
+	if len(key) == 0 {
+		return errors.New("key can not be empty")
+	}
+
+	// split 1 byte to 2 nibble.
+	hexPath := KeyToHex(key)
+
+	newRoot, err := t.insert(t.root, hexPath, value)
+	if err != nil {
+		return err
+	}
+	t.root = newRoot
+	return nil
+}
+
+// insert recursive insert node
+// MPT core algorithm, handle all kind of NodeType.
+func (t *ModifiedMerklePatriciaTree) insert(node *MPTNode, path []byte, value []byte) (*MPTNode, error) {
+	// 1. if nil node, create leaf node
+	if node == nil {
+		return &MPTNode{
+			NodeType: LEAF,
+			Path:     path,
+			Value:    value,
+			Dirty:    true,
+		}, nil
+	}
+
+	// mark as dirty node (require recalculate hash)
+	node.Dirty = true
+
+	switch node.NodeType {
+	case LEAF:
+		return t.insertIntoLeaf(node, path, value)
+	case EXTENSION:
+		return t.insertIntoExtension(node, path, value)
+	case BRANCH:
+		return t.insertIntoBranch(node, path, value)
+	default:
+		return nil, errors.New("invalid MPT node type")
+	}
+}
+
+// insertIntoLeaf Handle insert into LEAF node
+func (t *ModifiedMerklePatriciaTree) insertIntoLeaf(leaf *MPTNode, path []byte, value []byte) (*MPTNode, error) {
+	// calculate common prefix length
+	commonPathLen := commonPrefixLen(leaf.Path, path)
+
+	// ---------------------------------------------------------------------------
+	// S-1: All matched up: update node value
+	// ---------------------------------------------------------------------------
+
+	if commonPathLen == len(leaf.Path) && commonPathLen == len(path) {
+		leaf.Value = value
+		return leaf, nil
+	}
+
+	// ---------------------------------------------------------------------------
+	// S-2: Need split a new Node
+	// ---------------------------------------------------------------------------
+	newBranch := &MPTNode{ // create new branch
+		NodeType: BRANCH,
+		Dirty:    true,
+	}
+
+	// 2-1: no common path prefix >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	if commonPathLen == 0 {
+
+		// make 2 path to leaf and add into new branch.
+		// leaf-1:
+		originIdx := leaf.Path[0]
+		leaf.Path = leaf.Path[1:]
+		newBranch.Children[originIdx] = leaf
+		// leaf-2:
+		newIndex := path[0]
+		newBranch.Children[newIndex] = &MPTNode{
+			NodeType: LEAF,
+			Path:     path[1:],
+			Value:    value,
+			Dirty:    true,
+		}
+		return newBranch, nil
+	}
+
+	// 2-2: both Path have remaining >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	if commonPathLen < len(leaf.Path) && commonPathLen < len(path) {
+		// we need create a EXTENSION node to compress the path
+		extension := &MPTNode{
+			NodeType: EXTENSION,
+			Path:     leaf.Path[:commonPathLen],
+			Dirty:    true,
+			Child:    newBranch, // EXTENSION only have 1 child BRANCH.
+		}
+
+		// leaf-1:
+		originIdx := leaf.Path[commonPathLen]
+		leaf.Path = leaf.Path[commonPathLen+1:]
+		newBranch.Children[originIdx] = leaf
+		// leaf-2:
+		newIndex := path[commonPathLen]
+		newBranch.Children[newIndex] = &MPTNode{
+			NodeType: LEAF,
+			Path:     path[commonPathLen+1:],
+			Value:    value,
+			Dirty:    true,
+		}
+
+		return extension, nil
+	}
+
+	// 2-3: only origin leaf Path has remaining >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	if commonPathLen < len(leaf.Path) && commonPathLen == len(path) {
+		// we need create a EXTENSION node to compress the path
+		extension := &MPTNode{
+			NodeType: EXTENSION,
+			Path:     leaf.Path[:commonPathLen],
+			Dirty:    true,
+			Child:    newBranch, // EXTENSION only have 1 child BRANCH.
+		}
+
+		// leaf-1:
+		originIdx := leaf.Path[commonPathLen]
+		leaf.Path = leaf.Path[commonPathLen+1:]
+		newBranch.Children[originIdx] = leaf
+		// new path process:
+		newBranch.Value = value
+
+		return extension, nil
+	}
+
+	// 2-3: only new Path has remaining >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+	if commonPathLen == len(leaf.Path) && commonPathLen < len(path) {
+		// we need create a EXTENSION node to compress the path
+		extension := &MPTNode{
+			NodeType: EXTENSION,
+			Path:     leaf.Path[:commonPathLen],
+			Dirty:    true,
+			Child:    newBranch, // EXTENSION only have 1 child BRANCH.
+		}
+
+		// leaf-1:
+		newBranch.Value = leaf.Value
+		// leaf-2:
+		newIndex := path[commonPathLen]
+		newBranch.Children[newIndex] = &MPTNode{
+			NodeType: LEAF,
+			Path:     path[commonPathLen+1:],
+			Value:    value,
+			Dirty:    true,
+		}
+
+		return extension, nil
+	}
+
+	return nil, errors.New("invalid insert MPT leaf node conditions")
+}
+
+// insertIntoExtension Handle insert into EXTENSION node
+func (t *ModifiedMerklePatriciaTree) insertIntoExtension(ext *MPTNode, path []byte, value []byte) (*MPTNode, error) {
+	// TODO
+	return nil, nil
+}
+
+// insertIntoBranch Handle insert into BRANCH node
+func (t *ModifiedMerklePatriciaTree) insertIntoBranch(branch *MPTNode, path []byte, value []byte) (*MPTNode, error) {
+	// TODO
+	return nil, nil
+}
+
+// ========== Tool func ==========
+
+// commonPrefixLen calculate 2 path common path prefix len
+func commonPrefixLen(pathA []byte, pathB []byte) int {
+	minLen := min(len(pathA), len(pathB))
+
+	for i := 0; i < minLen; i++ {
+		if pathA[i] != pathB[i] {
+			return i
+		}
+	}
+	return minLen
+}
---
+++ crypto/mpt_test.go
@@ -0,0 +1,167 @@
+package crypto
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_HexToCompact_oddLeaf(t *testing.T) {
+	mockEvenPath := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}
+	// head: odd leaf: 0011 0000 | 0x01 = 0011 0001 = 0x31
+	// even leaf should be [0x31, 0x23, 0x45, 0x67]
+	res := HexToCompact(mockEvenPath, true)
+	assert.Equal(t, []byte{0x31, 0x23, 0x45, 0x67}, res)
+}
+
+func Test_HexToCompact_evenLeaf(t *testing.T) {
+	mockEvenPath := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06}
+	// head: even leaf: 0010 0000 = 0x20
+	// even leaf should be [0x20, 0x12, 0x34, 0x56]
+	res := HexToCompact(mockEvenPath, true)
+	assert.Equal(t, []byte{0x20, 0x12, 0x34, 0x56}, res)
+}
+
+func Test_HexToCompact_oddExt(t *testing.T) {
+	mockEvenPath := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}
+	// head: odd ext: 0001 0000 | 0x01 = 0001 0001 = 0x11
+	// odd ext should be [0x11, 0x23, 0x45, 0x67]
+	res := HexToCompact(mockEvenPath, false)
+	assert.Equal(t, []byte{0x11, 0x23, 0x45, 0x67}, res)
+}
+
+func Test_HexToCompact_evenExt(t *testing.T) {
+	mockEvenPath := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06}
+	// head: even dxt: 0000 0000 = 0x00
+	// even ext should be [0x00, 0x12, 0x34, 0x56]
+	res := HexToCompact(mockEvenPath, false)
+	assert.Equal(t, []byte{0x00, 0x12, 0x34, 0x56}, res)
+}
+
+func Test_CompactToHex_oddLeaf(t *testing.T) {
+	mockEvenPath := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}
+	// head: odd leaf: 0011 0000 | 0x01 = 0011 0001 = 0x31
+	// even leaf should be [0x31, 0x23, 0x45, 0x67]
+	comp := HexToCompact(mockEvenPath, true)
+	hex, isLeaf := CompactToHex(comp)
+	assert.True(t, isLeaf)
+	assert.Equal(t, mockEvenPath, hex)
+}
+
+func Test_CompactToHex_evenLeaf(t *testing.T) {
+	mockEvenPath := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06}
+	comp := HexToCompact(mockEvenPath, true)
+	hex, isLeaf := CompactToHex(comp)
+	assert.True(t, isLeaf)
+	assert.Equal(t, mockEvenPath, hex)
+}
+
+func Test_CompactToHex_oddExt(t *testing.T) {
+	mockEvenPath := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}
+	comp := HexToCompact(mockEvenPath, false)
+	hex, isLeaf := CompactToHex(comp)
+	assert.False(t, isLeaf)
+	assert.Equal(t, mockEvenPath, hex)
+}
+
+func Test_CompactToHex_evenExt(t *testing.T) {
+	mockEvenPath := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06}
+	comp := HexToCompact(mockEvenPath, false)
+	hex, isLeaf := CompactToHex(comp)
+	assert.False(t, isLeaf)
+	assert.Equal(t, mockEvenPath, hex)
+}
+
+func Test_KeyToHex(t *testing.T) {
+	key := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}
+	hex := KeyToHex(key)
+	fmt.Println(hex)
+	assert.Equal(t, []byte{0x00, 0x01, 0x00, 0x02, 0x00, 0x03, 0x00, 0x04, 0x00, 0x05, 0x00, 0x06, 0x00, 0x07}, hex)
+}
+
+func Test_Slice(t *testing.T) {
+	pathA := []byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}
+	pathB := []byte{0x01, 0x02, 0x03, 0x04, 0x08, 0x09, 0x0A}
+	commonLen := commonPrefixLen(pathA, pathB)
+	fmt.Println(commonLen)
+
+	remainingPath := pathA[commonLen+1:] // ex: a-b-c-d-e => d-e
+	fmt.Println(remainingPath)
+}
+
+func Test_MPT_Put_2DiffPath(t *testing.T) {
+	mpt := NewMPT()
+
+	key_1 := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
+	key_2 := []byte{0x11, 0x12, 0x13, 0x14, 0x15}
+	err := mpt.Put(key_1, key_1)
+	assert.Nil(t, err)
+	fmt.Println(mpt.root)
+
+	err = mpt.Put(key_2, key_2)
+	assert.Nil(t, err)
+
+	fmt.Println("==============================================")
+	fmt.Println("root: ", mpt.root)
+
+	fmt.Println("first leaf: ", mpt.root.Children[0])
+	fmt.Println("sec leaf: ", mpt.root.Children[1])
+}
+
+func Test_MPT_Put_bothPathHaveRemaining(t *testing.T) {
+	mpt := NewMPT()
+
+	key_1 := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
+	key_2 := []byte{0x01, 0x02, 0x13, 0x14, 0x15}
+	err := mpt.Put(key_1, key_1)
+	assert.Nil(t, err)
+	fmt.Println(mpt.root)
+
+	err = mpt.Put(key_2, key_2)
+	assert.Nil(t, err)
+
+	fmt.Println("==============================================")
+	fmt.Println("root: ", mpt.root)
+	fmt.Println("child-branch: ", mpt.root.Child)
+
+	fmt.Println("first leaf: ", mpt.root.Child.Children[0])
+	fmt.Println("sec leaf: ", mpt.root.Child.Children[1])
+}
+
+func Test_MPT_Put_OriginLeafHasRemaining(t *testing.T) {
+	mpt := NewMPT()
+
+	key_1 := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
+	key_2 := []byte{0x01, 0x02, 0x03}
+	err := mpt.Put(key_1, key_1)
+	assert.Nil(t, err)
+	fmt.Println(mpt.root)
+
+	err = mpt.Put(key_2, key_2)
+	assert.Nil(t, err)
+
+	fmt.Println("==============================================")
+	fmt.Println("root: ", mpt.root)
+	fmt.Println("child-branch: ", mpt.root.Child)
+
+	fmt.Println("first leaf: ", mpt.root.Child.Children[0])
+}
+
+func Test_MPT_Put_NewPathHasRemaining(t *testing.T) {
+	mpt := NewMPT()
+
+	key_1 := []byte{0x01, 0x02, 0x03}
+	key_2 := []byte{0x01, 0x02, 0x03, 0x04, 0x05}
+	err := mpt.Put(key_1, key_1)
+	assert.Nil(t, err)
+	fmt.Println(mpt.root)
+
+	err = mpt.Put(key_2, key_2)
+	assert.Nil(t, err)
+
+	fmt.Println("==============================================")
+	fmt.Println("root: ", mpt.root)
+	fmt.Println("child-branch: ", mpt.root.Child)
+	fmt.Println("first: ", mpt.root.Child.Children[0])
+
+}
